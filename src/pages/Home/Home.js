1a4c4154694b08eea2e4681281e5a48b3dac530c
anisul islam <anisulislam980340@gmail.com>
2022-12-24 19:13:12 +0600

    setup food section one with sucessfully

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
 import Banner from './Banner';
+import SectionOne from './SectionOne';
 
 const Home = () => {
     return (
         <div>            
             <Banner></Banner>
+            <SectionOne></SectionOne>
            
         </div>
     );
---
+++ src/pages/Home/SectionOne.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from '../../assets/food.jpg'
+
+const SectionOne = () => {
+    return (
+        <div className="hero">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={image} className="rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">THE BEST HEALTHY FOOD</h1>
+      <p className="py-6">If you talk about food just as much as you eat it, you're in good company. We've got famous chefs and stars</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default SectionOne;
