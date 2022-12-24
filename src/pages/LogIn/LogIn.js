e862b485b3317c6ab5774bf9a6665865572ce442
anisul islam <anisulislam980340@gmail.com>
2022-12-24 20:50:14 +0600

    find login info in the console

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+  const handleLogIn = event =>{
+    event.preventDefault()
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user ={email, password}
+
+    console.log(user)
+    form.reset()
+  }
+  
     return (
         <div className="hero">
         <div className="hero-content flex-col">
@@ -10,18 +21,18 @@ const LogIn = () => {
             
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
+            <form onSubmit={handleLogIn} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="text" placeholder="email" name='email' className="input input-bordered" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input type="text" placeholder="password" name='password' className="input input-bordered" />
                 <label className="label">
                   <p className="label-text-alt">Create a account..   <Link className='text-orange-500' to='/register'>Register</Link></p>
                 </label>
@@ -29,7 +40,7 @@ const LogIn = () => {
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
