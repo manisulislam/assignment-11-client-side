56060f7c2b9c35cf5e1b7ed8fac6e1a4e9aed181
anisul islam <anisulislam980340@gmail.com>
2022-12-24 20:42:48 +0600

    setup log in

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
     return (
-        <div>
-            <h4>this is login</h4>
+        <div className="hero">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="email" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="text" placeholder="password" className="input input-bordered" />
+                <label className="label">
+                  <p className="label-text-alt">Create a account..   <Link className='text-orange-500' to='/register'>Register</Link></p>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
