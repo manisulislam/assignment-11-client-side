83f206f6750cd8c6509ae1b2f94a26f0f04e7aa0
anisul islam <anisulislam980340@gmail.com>
2022-12-26 07:46:27 +0600

    complete full setup in six card

--- src/pages/Home/ServiceDetail.js
+++ src/pages/Home/ServiceDetail.js
@@ -5,10 +5,10 @@ const ServiceDetail = ({ allService }) => {
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl m-5">
-            <figure><img src={image} alt="Shoes" /></figure>
+            <figure><img className='w-44' src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>{details}</p>
+                <p>{details.length>100? details.slice(0,100) + '...': details}</p>
                 <p>price: ${price}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">view details</button>
