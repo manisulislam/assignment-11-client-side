cb8a45c54c501d501c9f0cb4025d62deed1e33bf
anisul islam <anisulislam980340@gmail.com>
2022-12-27 22:09:52 +0600

    finish condiational rendering in myreview page

--- src/pages/MyReviews/MyReview.js
+++ src/pages/MyReviews/MyReview.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { VscAccount } from "react-icons/vsc";
+
 
 const MyReview = ({ option }) => {
     const { serviceName, reviewerPhoto, name, reviewerName } = option
     return (
-        <div className="card w-72 shadow-xl">
+        <div className="card w-72 my-12 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title"><span className='text-lg text-orange-400'>Service Name:</span> {serviceName}</h2>
                 <div className='flex   justify-between items-center'>
                     <div className="avatar">
-                        <div className="w-24 pt-5 rounded-full">
-                            <img src={reviewerPhoto} />
+                        <div className="w-24 p-12 rounded-md">
+                            {!reviewerPhoto == 0 ? <VscAccount className=''></VscAccount>:<img src={reviewerPhoto} alt=''/>}
                         </div>
                     </div>
                     <h1>{reviewerName}</h1>
