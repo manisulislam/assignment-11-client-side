a1cb83e5aaa120ee302651d8623e79ee0fe354b8
anisul islam <anisulislam980340@gmail.com>
2023-01-02 21:17:22 +0600

    setup project sucessfully

--- src/pages/MyReviews/MyReview.js
+++ src/pages/MyReviews/MyReview.js
@@ -3,36 +3,20 @@ import { VscAccount } from "react-icons/vsc";
 import { Link } from 'react-router-dom';
 
 
-const MyReview = ({ myReviews, option,displayReview,setDisplayReview }) => {
+const MyReview = ({ myReviews, option, handleDelete}) => {
+    const [test, setTest]= useState(false)
     
-    console.log(myReviews)
+    console.log ('my', myReviews)
     const {_id, serviceName, reviewerPhoto, name, reviewerName } = option
-    const handleDelete=(option)=>{
-        console.log('deleting id ', option._id)
-        fetch(`http://localhost:5000/myReview/${option._id}`,{
-            method: 'DELETE'
-        })
-        .then(res=> res.json())
-        .then(data => {
-            console.log(data)
-            if(data.deletedCount > 0){
-                alert('deleted sucessfully')
-                const remainignReview = displayReview.filter(review => review._id!==option._id)
-                setDisplayReview(remainignReview)
-
-            }
-
-        })
-
-    }
+   
 
     return (
 
     
 
         <div>
-            { 
-            (!myReviews.length === 0) ?<div className="card w-72 my-12 shadow-xl">
+             { 
+            myReviews ?<div className="card w-72 my-12 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title"><span className='text-lg text-orange-400'>Service Name:</span> {serviceName}</h2>
                 <div className='flex   justify-between items-center'>
@@ -44,15 +28,17 @@ const MyReview = ({ myReviews, option,displayReview,setDisplayReview }) => {
                     <h1>{reviewerName}</h1>
                 </div>
                 <p> <span className='text-1xl text-orange-400'>Our Customer review:</span> {name}</p>
-                <Link><button onClick={()=>handleDelete(option)} className="btn btn-outline btn-primary w-full">DELETE</button></Link>
+                <Link><button onClick={()=>handleDelete(_id)} className="btn btn-outline btn-primary w-full">DELETE</button></Link>
                 <Link to={`/update/${_id}`}><button className="btn btn-outline btn-secondary w-full">EDIT</button></Link>
 
 
             </div>
+            hello
         </div> : <p className='text-3xl text-center my-64 font-semibold'> No reviews  were added</p>
             
             
             }
+           
            
         </div>
     );
--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -6,15 +6,35 @@ import MyReview from './MyReview';
 
 const MyReviews = () => {
     const [myReviews, setMyReviews]= useState([])
-    const [displayReview, setDisplayReview] = useState(myReviews)
+    const [control, setControl]= useState(false)
+
     useEffect(()=>{
         fetch('http://localhost:5000/myReviews')
         .then(res=> res.json())
         .then(data=> {
             setMyReviews(data)
         })
-    },[])
+    },[control])
     useDocumentTitle('GOOD CHOICE/myreviews')
+    const handleDelete=(id)=>{
+        console.log('deleting id ',id)
+        fetch(`http://localhost:5000/myReview/${id}`,{
+            method: 'DELETE'
+        })
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data)
+            if(data.deletedCount > 0){
+                setControl(!control)
+                alert('deleted sucessfully')
+                // const remainignReview = displayReview.filter(review => review._id!==option._id)
+                // setDisplayReview(remainignReview)
+
+            }
+
+        })
+
+    }
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             
@@ -22,10 +42,10 @@ const MyReviews = () => {
                 myReviews.map(option => <MyReview
                 key={option._id}
                 option={option}
+                handleDelete={handleDelete}
                 myReviews={myReviews}
-                displayReview={displayReview}
                 
-                setDisplayReview={setDisplayReview}
+               
                 
                 ></MyReview>)
             }
